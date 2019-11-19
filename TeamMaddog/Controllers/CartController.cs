ab147ca644a91cc989936e8edd4efb2dc473e7ed
Paige Harper <paige.e.harper@gmail.com>
2019-11-19 12:37:10 -0500

    cartcontroller

--- TeamMaddog/Controllers/CartController.cs
+++ TeamMaddog/Controllers/CartController.cs
@@ -3,16 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TeamMaddog.Models;
 
 namespace TeamMaddog.Controllers
 {
     public class CartController : Controller
     {
-        // GET: Cart
-        public ActionResult Index()
+        //initialize customer list
+        public static List<Cart> lstCartItems = new List<Cart>();
+
+        //show cart items function displays cart items
+        public ActionResult ShowCartItems()
+        {
+            return View(lstCartItems);
+        }
+
+        //get info from addcartitems view
+        [HttpGet]
+        public ActionResult AddCartItems()
         {
             return View();
         }
 
+        //post info to showcartitems view
+        [HttpPost]
+        public ActionResult AddCartItems(Cart myCartItems)
+        {
+            if (ModelState.IsValid)
+            {
+                lstCartItems.Add(myCartItems);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View(myCartItems);
+            }
+        }
     }
 }
