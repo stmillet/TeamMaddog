69015c070fcdb8c2c5abcde9b29110f17988cf76
Emmett Madsen <emmett1013@gmail.com>
2019-11-19 20:43:55 -0700

    Revert "add comments"
    This reverts commit 5fe2fd4cc46d4307e3fcc59082bfb7288486c34c.

--- TeamMaddog/Models/Product.cs
+++ TeamMaddog/Models/Product.cs
@@ -8,21 +8,17 @@ namespace TeamMaddog.Models
 {
     public class Product
     {   
-        //initialize productID as primary key
         [Key]
         public int ProductID { get; set; }
 
-        //initialize product name
         [Required]
         [Display(Name = "Item")]
         public string ProdName { get; set; }
 
-        //initialize product price
         [Required]
         [Display(Name = "Price")]
         public double ProdPrice { get; set; }
 
-        //initialize product size
         [Display(Name = "Size")]
         public string ProdSize { get; set; }
 
