da9806bf114f49c0c70ba4d4ed406584253d0794
Paige Harper <paige.e.harper@gmail.com>
2019-11-19 22:12:02 -0500

    add customer

--- TeamMaddog/Models/Customer.cs
+++ TeamMaddog/Models/Customer.cs
@@ -16,13 +16,11 @@ namespace TeamMaddog.Models
         //initialize customer first name
         [Required]
         [Display(Name = "First Name")]
-        [StringLength(25, ErrorMessage = "Name must be less than 25 characters")]
         public string Customer_FName { get; set; }
 
         //initialize customer last name
         [Required]
         [Display(Name = "Last Name")]
-        [StringLength(25, ErrorMessage = "Name must be less than 25 characters")]
         public string Customer_LName { get; set; }
 
         //initialize customer email
@@ -34,25 +32,21 @@ namespace TeamMaddog.Models
         //initialize customer street address
         [Required]
         [Display(Name = "Street Address")]
-        [StringLength(50, ErrorMessage = "Street Address must be less than 50 characters")]
         public string Cust_Street_Address { get; set; }
 
         //initialize customer  city
         [Required]
         [Display(Name = "City")]
-        [StringLength(20, ErrorMessage = "City must be less than 20 characters")]
         public string Cust_City { get; set; }
 
         //initialize customer state
         [Required]
         [Display(Name = "State")]
-        [StringLength(2, MinimumLength = 2, ErrorMessage = "State Abbreviation must be 2 characters")]
         public string Cust_State { get; set; }
 
         //initialize customer zip
         [Required]
         [Display(Name = "Zip")]
-        [StringLength(5, MinimumLength = 5, ErrorMessage = "Zip must be 5 characters")]
         public int Cust_Zip { get; set; }
     }
 }
